46fc53b3a50131cb24d8e0c22b533eb37a78777a
Vasily Krylov <tor@outsorcerers.com>
2017-03-09 01:56:32 +0300

    Add path

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function readdir(path, callback) {
             }
           })
         } else {
-          list.push(stats)
+          list.push(Object.assign({paht: filePath}, stats));
           pending -= 1
           if (!pending) {
             return callback(null, list)
